2e7aae02e4ceaaf310a4eb64a9bfb02eee4b8d9e
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-10 18:00:11 +0100

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,7 @@
+user_number = int(input("enter a number: ")) # 10
+user_length = int(input("enter a length: ")) # 5
+i = 1
+while i <= user_length:
+    the_calculation = user_number * i
+    print(the_calculation)
+    i += 1
